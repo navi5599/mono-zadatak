5f3adc1085486c5786cade5858c504f1704ec1db
navi5599 <ivkelava10@gmail.com>
2023-02-13 10:01:18 +0100

    added functionality to sort models

--- src/stores/SideBarStore.js
+++ src/stores/SideBarStore.js
@@ -1,26 +1,61 @@
 import { observable, action, makeObservable } from 'mobx';
+import globalStore from '../common/stores/GlobalStore';
+
+//Helper function to sort models by price
+const sortModelsByPrice = (sortType) => {
+  const currentModels = globalStore.models;
+  const sortedModels = currentModels.sort((a, b) => {
+    let priceA = parseInt(a.price);
+    let priceB = parseInt(b.price);
+    if (sortType === 'asce') {
+      return priceA - priceB;
+    } else if (sortType === 'desc') {
+      return priceB - priceA;
+    }
+    return currentModels;
+  });
+
+  return (globalStore.models = sortedModels);
+};
+
+// MobX store implementation
 
 class SideBarStore {
   searchedCar = '';
+
   showFilter = false;
   showSort = false;
+
+  showNameOptionSort = false;
+  showPriceOptionSort = false;
 
   constructor() {
     makeObservable(this, {
       searchedCar: observable,
       showFilter: observable,
       showSort: observable,
-      handleFilter: action,
-      handleSort: action,
+      showNameOptionSort: observable,
+      showPriceOptionSort: observable,
+      handleFilterAndSort: action,
+      handleSortOptions: action,
+      sortModels: action,
     });
   }
 
-  handleFilter = () => {
-    this.showFilter = !this.showFilter;
+  handleSortOptions = (id) => {
+    if (id === 'sort_name')
+      return (this.showNameOptionSort = !this.showNameOptionSort);
+    if (id === 'price_name')
+      return (this.showPriceOptionSort = !this.showPriceOptionSort);
   };
 
-  handleSort = () => {
-    this.showSort = !this.showSort;
+  handleFilterAndSort = (id) => {
+    if (id === 'filter') return (this.showFilter = !this.showFilter);
+    if (id === 'sort') return (this.showSort = !this.showSort);
+  };
+
+  sortModels = (sortType) => {
+    sortModelsByPrice(sortType);
   };
 }
 
