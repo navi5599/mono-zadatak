c2bd26f161f4b41eacdc67b614635254e5fd2d9e
navi5599 <ivkelava10@gmail.com>
2023-03-07 11:21:19 +0100

    added logout button

--- src/layouts/NavBar/NavBar.js
+++ src/layouts/NavBar/NavBar.js
@@ -4,12 +4,25 @@ import { Link } from 'react-router-dom';
 import './NavBar.css';
 
 function NavBar() {
+  const token = localStorage.getItem('token');
+
+  const handleLogout = () => {
+    localStorage.clear();
+    window.open('/', '_self');
+  };
   return (
     <div className="navbar">
       <Link to="/" style={{ textDecoration: 'none' }}>
         <h3>Showcar</h3>
       </Link>
       <p>すべての人が平和に暮らしていることを想像してみてください</p>
+      {token ? (
+        <h4 className="logout" onClick={handleLogout}>
+          Log out
+        </h4>
+      ) : (
+        ''
+      )}
     </div>
   );
 }
